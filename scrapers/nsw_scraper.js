ac02c3b12a44755ba94503f30531ddd2bd47e6a7
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-09-12 01:42:10 +0530

    nsw  -scraper finalized

--- scrapers/nsw_scraper.js
+++ scrapers/nsw_scraper.js
@@ -49,12 +49,76 @@ async function scrapeData() {
           return headline ? headline.textContent.trim() : null;
         });
 
-        // Add the scraped data to the overall list
+        let idNumber = await page.evaluate(() => {
+          const id = document.querySelector(
+            "#RFT-SB-ID > span.sidebarBlock-content"
+          );
+          return id ? id.textContent.trim() : null;
+        });
+
+        idNumber = "new-" + idNumber;
+
+        const closingDate = await page.evaluate(() => {
+          const date = document.querySelector(
+            "#RFT-SB-Closes > span.sidebarBlock-content"
+          );
+          return date ? date.textContent.trim() : null;
+        });
+
+        const publishedDate = await page.evaluate(() => {
+          const date = document.querySelector(
+            "#RFT-SB-Published > span.sidebarBlock-content"
+          );
+          return date ? date.textContent.trim() : null;
+        });
+
+        const category = await page.evaluate(() => {
+          const category = document.querySelector(
+            "#RFT-SB-Category > span.sidebarBlock-content"
+          );
+          return category ? category.textContent.trim() : null;
+        });
+
+        // Check if the description selector exists on the page
+        const description = await page.evaluate(() => {
+          const desData = document.querySelector(".richtextblock-content");
+          return desData ? desData.textContent.trim() : null;
+        });
+
+        // Check if the regions selector exists on the page
+        const regionElement = await page.$("#RFT-Location");
+        const regions = regionElement
+          ? await page.$eval("#RFT-Location", (regionElement) => {
+              const regionsText = regionElement.textContent.trim();
+              // Split the text by commas and trim each region
+              const regionsArray = regionsText
+                .split(",")
+                .map((region) => region.trim());
+              return regionsArray;
+            })
+          : null;
+
+        const Location = "NSW";
+
+        // Add the scraped data to the overall list if title exists
         if (title) {
-          allData.push({
+          const data = {
             link,
             title,
-          });
+            idNumber,
+            closingDate,
+            publishedDate,
+            category,
+            Location,
+            description,
+          };
+
+          // Add regions if they exist
+          if (regions) {
+            data.regions = regions;
+          }
+
+          allData.push(data);
         }
       }
 
