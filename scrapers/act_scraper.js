06a1cb883a10ed5a11845184b804da67191d2af5
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-09-11 10:24:10 +0530

    act -full scraper

--- scrapers/act_scraper.js
+++ scrapers/act_scraper.js
@@ -35,22 +35,93 @@ async function act_ScraperData() {
           .filter((href) =>
             href.startsWith("https://www.tenders.act.gov.au/tender/")
           );
+
         return filteredLinks;
       });
 
       for (const link of links) {
+        // Visit each link individually
         await page.goto(link);
 
-        // Wait for the title element to load
-        await page.waitForSelector("#tenderTitle");
+        // Set the opening date to the current date
+        const publishedDate = new Date().toLocaleDateString("en-GB", {
+          day: "numeric",
+          month: "short",
+          year: "numeric",
+        });
 
-        // Extract the title
+        // Extract the closing date from the page and clean it
+        const closingDateRaw = await page.$eval(
+          "#tenderClosingTime",
+          (dateElement) => dateElement.textContent.trim()
+        );
+        const closingDateMatch = closingDateRaw.match(
+          /(\d{1,2} \w+ \d{4} \d{2}:\d{2} [apAP][mM])/
+        );
+        const closingDate = closingDateMatch
+          ? closingDateMatch[0]
+          : "Not specified";
+
+        // Format closing date to "30 June 2025" format
+        const formattedClosingDate = closingDate
+          .split(" ")
+          .slice(0, 3)
+          .join(" ");
+
+        // Extract the title from the link page
         const title = await page.$eval("#tenderTitle", (titleElement) =>
           titleElement.textContent.trim()
         );
+        const idNumber = await page.$eval(
+          "#opportunityGeneralDetails > div:nth-child(4) > div.col-sm-9.col-md-10",
+          (idNumberElement) =>
+            idNumberElement.textContent
+              .trim()
+              .replace(/[\t\n]/g, " ") // Remove tabs and newlines
+              .replace(/\s+/g, " ") // Replace multiple spaces with a single space
+              .trim()
+        );
 
-        // Push the title and link to the data array
-        allData.push({ title, link });
+        const category = await page.$eval(
+          "#opportunityGeneralDetails > div:nth-child(5)",
+          (categoryElement) =>
+            categoryElement.textContent
+              .trim()
+              .replace(/[\t\n]/g, " ") // Remove tabs and newlines
+              .replace(/\s+/g, " ") // Replace multiple spaces with a single space
+              .trim()
+        );
+
+        const description = await page.$eval(
+          "#tenderDescription > div:nth-child(2)",
+          (descriptionElement) =>
+            descriptionElement.textContent
+              .trim()
+              .replace(/[\t\n]/g, " ") // Remove tabs and newlines
+              .replace(/\s+/g, " ") // Replace multiple spaces with a single space
+              .trim()
+        );
+
+        const location = "ACT";
+        const region = [];
+        const updatedDateTime = new Date().toLocaleDateString("en-GB", {
+          day: "numeric",
+          month: "short",
+          year: "numeric",
+        });
+        // Push the title, link, opening date, and formatted closing date to the data array
+        allData.push({
+          title,
+          link,
+          publishedDate,
+          closingDate: formattedClosingDate,
+          idNumber,
+          category,
+          description,
+          location,
+          region,
+          updatedDateTime,
+        });
       }
     }
 
